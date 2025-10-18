ed6abf3060054c8c6a66e1f3478eb3be96b67a85
jpmitmpr <jpmitmpr@gmail.com>
2025-10-18 19:18:36 -0400

    Debugged final output issues

--- doctor_tree.py
+++ doctor_tree.py
@@ -1,12 +1,155 @@
-class DoctorNode:
-    pass # Delete this line and implement the DoctorNode class
-
-
-
-class DoctorTree:
-    pass # Delete this line and implement the DoctorTree class
+# class DoctorNode:
+#     pass
+#     def __init__(self, name):
+#         self.name = name
+#         self.left = None
+#         self.right = None
+# 
+# 
+# class DoctorTree:
+#     passS
+#     def __init__(self):
+#         self.root = None
+# 
+#     def insert(self, parent_name, child_name, side):
+#         """Insert a new DoctorNode under a given parent on the given side ('left' or 'right')."""
+#         if side not in ("left", "right"):
+#             raise ValueError("Side must be 'left' or 'right'.")
+# 
+#         parent_node = self._find_node(self.root, parent_name)
+#         if parent_node is None:
+#             raise ValueError(f"Parent doctor '{parent_name}' not found in the tree.")
+# 
+#         new_node = DoctorNode(child_name)
+#         if side == "left":
+#             if parent_node.left is not None:
+#                 raise ValueError(f"Parent '{parent_name}' already has a left report.")
+#             parent_node.left = new_node
+#         else:
+#             if parent_node.right is not None:
+#                 raise ValueError(f"Parent '{parent_name}' already has a right report.")
+#             parent_node.right = new_node
+# 
+#     def _find_node(self, node, name):
+#         """Helper to find a doctor by name."""
+#         if node is None:
+#             return None
+#         if node.name == name:
+#             return node
+# 
+#         left_result = self._find_node(node.left, name)
+#         if left_result:
+#             return left_result
+# 
+#         return self._find_node(node.right, name)
+# 
+#     def preorder(self, node):
+#         """Root -> Left -> Right"""
+#         if node is None:
+#             return []
+#         return [node.name] + self.preorder(node.left) + self.preorder(node.right)
+# 
+#     def inorder(self, node):
+#         """Left -> Root -> Right"""
+#         if node is None:
+#             return []
+#         return self.inorder(node.left) + [node.name] + self.inorder(node.right)
+# 
+#     def postorder(self, node):
+#         """Left -> Right -> Root"""
+#         if node is None:
+#             return []
+#         return self.postorder(node.left) + self.postorder(node.right) + [node.name]
+# 
+# if __name__ == "__main__":
+#     tree = DoctorTree()
+#     tree.root = DoctorNode("Dr. John")
+#     tree.insert("Dr. John", "Dr. Sam", "right")
+#     tree.insert("Dr. John", "Dr. Jose", "left")
+#     tree.insert("Dr. Samclea", "Dr. Robert", "right")
+#     tree.insert("Dr. Sam", "Dr. Gray", "left")
+# 
+#     print("Preorder:", tree.preorder(tree.root))
+#     print("Inorder:", tree.inorder(tree.root))
+#     print("Postorder:", tree.postorder(tree.root))
 
 
 
 
 # Test your DoctorTree and DoctorNode classes here
+
+class DoctorNode:
+
+    def __init__(self, name):
+        self.name = name
+        self.left = None
+        self.right = None
+
+
+
+class DoctorTree:
+
+    def __init__(self):
+        self.root = None
+
+    def insert(self, parent_name, child_name, side):
+        """Insert a new DoctorNode under a given parent on the given side ('left' or 'right')."""
+        if side not in ("left", "right"):
+            raise ValueError("Side must be 'left' or 'right'.")
+
+        parent_node = self._find_node(self.root, parent_name)
+        if parent_node is None:
+            raise ValueError(f"Parent doctor '{parent_name}' not found in the tree.")
+
+        new_node = DoctorNode(child_name)
+        if side == "left":
+            if parent_node.left is not None:
+                raise ValueError(f"Parent '{parent_name}' already has a left report.")
+            parent_node.left = new_node
+        else:
+            if parent_node.right is not None:
+                raise ValueError(f"Parent '{parent_name}' already has a right report.")
+            parent_node.right = new_node
+
+    def _find_node(self, node, name):
+        """Helper to find a doctor by name."""
+        if node is None:
+            return None
+        if node.name == name:
+            return node
+
+        left_result = self._find_node(node.left, name)
+        if left_result:
+            return left_result
+
+        return self._find_node(node.right, name)
+
+    def preorder(self, node):
+        """Root -> Left -> Right"""
+        if node is None:
+            return []
+        return [node.name] + self.preorder(node.left) + self.preorder(node.right)
+
+    def inorder(self, node):
+        """Left -> Root -> Right"""
+        if node is None:
+            return []
+        return self.inorder(node.left) + [node.name] + self.inorder(node.right)
+
+    def postorder(self, node):
+        """Left -> Right -> Root"""
+        if node is None:
+            return []
+        return self.postorder(node.left) + self.postorder(node.right) + [node.name]
+
+if __name__ == "__main__":
+    tree = DoctorTree()
+    tree.root = DoctorNode("Dr. John")
+    tree.insert("Dr. John", "Dr. Sam", "right")
+    tree.insert("Dr. John", "Dr. Jose", "left")
+    tree.insert("Dr. Sam", "Dr. Robert", "right")
+    tree.insert("Dr. Sam", "Dr. Gray", "left")
+
+    print("Preorder:", tree.preorder(tree.root))
+    print("Inorder:", tree.inorder(tree.root))
+    print("Postorder:", tree.postorder(tree.root))
--- emergency_queue.py
+++ emergency_queue.py
@@ -1,12 +1,155 @@
-class Patient:
-    pass # Delete this line and implement the Patient class
+# "class Patient":
+
+#   def __init__(self, name, urgency):
+#       self.name = name
+#       self.urgency = urgency  # 1 is most urgent (min value)
+
+#   def __repr__(self):
+#        return f"{self.name} ({self.urgency})"
 
 
 
-class MinHeap:
-    pass # Delete this line and implement the MinHeap class
+# "class MinHeap":
 
+#    def __init__(self):
+#       self.data = []
+
+#    def heapify_up(self, index):
+#        """Move the patient up until the heap property is restored."""
+#        parent_index = (index - 1) // 2
+#        while index > 0 and self.data[index].urgency < self.data[parent_index].urgency:
+#            index = parent_index
+#            parent_index = (index - 1) // 2
+
+#    def heapify_down(self, index):
+#        """Move the patient down until the heap property is restored."""
+#        size = len(self.data)
+#        while True:
+#            left = 2 * index + 1
+#            right = 2 * index + 2
+#            smallest = index
+
+#               smallest = left
+#            if right < size and self.data[right].urgency < self.data[smallest].urgency:
+#                smallest = right
+
+#            if smallest == index:
+#                break
+
+#            self.data[index], self.data[smallest] = self.data[smallest], self.data[index]
+#            index = smallest
+
+#    def insert(self, patient):
+#        """Add a new patient and restore heap order."""
+#        self.data.append(patient)
+#        self.heapify_up(len(self.data) - 1)
+
+#    def peek(self):
+#        """Return the most urgent patient without removing them."""
+#        if not self.data:
+#            return None
+#        return self.data[0]
+
+#    def remove_min(self):
+#        """Remove and return the most urgent patient."""
+#        if not self.data:
+#            return None
+#        if len(self.data) == 1:
+#            return self.data.pop()
+
+#        root = self.data[0]
+#       self.data[0] = self.data.pop()
+#        self.heapify_down(0)
+#        return root
+
+#    def print_heap(self):
+#        print("Current Queue:")
+#        if not self.data:
+#            print("- (empty)")
+#           return
+#        for patient in self.data:
+#        print(f"- {patient.name} ({patient.urgency})")
 
 
 
 # Test your MinHeap class here including edge cases
+
+class Patient:
+    def __init__(self, name, urgency):
+        self.name = name
+        self.urgency = urgency  # 1 is most urgent (min value)
+
+    def __repr__(self):
+        return f"{self.name} ({self.urgency})"
+
+
+class MinHeap:
+    def __init__(self):
+        self.data = []
+
+    def heapify_up(self, index):
+        """Move the patient up until the heap property is restored."""
+        parent_index = (index - 1) // 2
+        while index > 0 and self.data[index].urgency < self.data[parent_index].urgency:
+            self.data[index], self.data[parent_index] = self.data[parent_index], self.data[index]
+            index = parent_index
+            parent_index = (index - 1) // 2
+
+    def heapify_down(self, index):
+        """Move the patient down until the heap property is restored."""
+        size = len(self.data)
+        while True:
+            left = 2 * index + 1
+            right = 2 * index + 2
+            smallest = index
+
+            if left < size and self.data[left].urgency < self.data[smallest].urgency:
+                smallest = left
+            if right < size and self.data[right].urgency < self.data[smallest].urgency:
+                smallest = right
+
+            if smallest == index:
+                break
+
+            self.data[index], self.data[smallest] = self.data[smallest], self.data[index]
+            index = smallest
+
+    def insert(self, patient):
+        """Add a new patient and restore heap order."""
+        self.data.append(patient)
+        self.heapify_up(len(self.data) - 1)
+
+    def peek(self):
+        """Return the most urgent patient without removing them."""
+        if not self.data:
+            return None
+        return self.data[0]
+
+    def remove_min(self):
+        """Remove and return the most urgent patient."""
+        if not self.data:
+            return None
+        if len(self.data) == 1:
+            return self.data.pop()
+
+        root = self.data[0]
+        self.data[0] = self.data.pop()
+        self.heapify_down(0)
+        return root
+
+    def print_heap(self):
+        print("Current Queue:")
+        for patient in self.data:
+            print(f"- {patient.name} ({patient.urgency})")
+
+if __name__ == "__main__":
+    heap = MinHeap()
+    heap.insert(Patient("Juan", 2))
+    heap.insert(Patient("Pablo", 1))
+    heap.insert(Patient("Morales", 3))
+    heap.print_heap()
+
+    print("\nNext up:", heap.peek())
+    served = heap.remove_min()
+    print("\nServed:", served)
+    heap.print_heap()
